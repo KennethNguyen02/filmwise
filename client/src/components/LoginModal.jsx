dd05b0122238825b4a170b564be2ec6f0df60c67
Kenneth <kenneth@Kenneth-sin-MacBook-Air.local>
2026-03-26 12:54:04 +0900

    feat: added input field for login and sign up in login modal

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,12 +1,36 @@
 
-import {Modal, ModalContent, ModalFooter, ModalHeader, Button} from '@heroui/react'
-
+import {Modal, ModalContent, ModalFooter, ModalBody,  ModalHeader, Button} from '@heroui/react';
+import {Tabs, Tab, Input} from '@heroui/react';
 export default function LoginModal({isOpen, onOpenChange}){
     return (
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false}>
             <ModalContent>
                 <ModalHeader>
-                </ModalHeader>
+                   
+
+               </ModalHeader>
+               <ModalBody>
+                    <div className = "flex justify-center flex-col">
+                        <Tabs fullWidth color="primary">
+                            <Tab key="login" title="Log in" >
+                                <div className="flex flex-col gap-2">
+                                    <label className="text-foreground" htmlFor="input-type-username">Username</label>
+                                    <Input id="input-type-username" placeholder="PeterSmith32" type="text" />
+
+                                    <label htmlFor="input-type-password">Password</label>
+                                    <Input id="input-type-password" placeholder="••••••••" type="password" />
+                                </div>
+                            </Tab>
+
+                            <Tab key="signup" title="Sign up" >
+                                <div className="flex flex-col gap-1">
+                                    <label htmlFor="input-type-password">Password</label>
+                                    <Input id="input-type-password" placeholder="••••••••" type="password" />
+                                </div>
+                            </Tab>
+                        </Tabs>
+                    </div>
+               </ModalBody>
                 <ModalFooter>
                     <Button> </Button>
                 </ModalFooter>
